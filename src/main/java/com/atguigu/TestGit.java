3dbb8df848d6094ea6f7c285c47e3b0a3802fc65
ymy <1316165298@qq.com>
2021-06-09 18:42:33 +0800

    2.第一次分支测试 3.hotfix提交 4.master解决冲突测试

--- src/main/java/com/atguigu/TestGit.java
+++ src/main/java/com/atguigu/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("master");
+        System.out.println("master!!!");
         System.out.println("hotfix");
     }
 }
